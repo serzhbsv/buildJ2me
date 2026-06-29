ce3def28a956fd90390131b3d51c154ea06ee8bd
Serzh5 <sepzh@rambler.ru>
2026-06-29 15:41:43 +0300

    Update build.sh

--- J2meSrc/pmjparser/build.sh
+++ J2meSrc/pmjparser/build.sh
@@ -48,11 +48,4 @@ ${PREVERIFY} \
 
 echo "Jaring preverified class files..."
 ${JAR} cmf MANIFEST.MF ${FontViewer}.jar -C ./classes .
-
-if [ -e font.pmj ] ; then
-  ${JAR} uf ${FontViewer}.jar font.pmj .
-fi
-
-echo
-echo "Don't forget to update the JAR file size in the JAD file!!!"
-echo
+${JAR} uf ${FontViewer}.jar font.pmj
